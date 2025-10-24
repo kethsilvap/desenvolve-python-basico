3da6c1326d29ea7c284a9ab09f9da9f05b85ef7f
Kethelyn Silva <kethelynsilvapinto0@gmail.com>
2025-10-24 09:39:49 -0300

    Add files via upload

---
+++ Modulo 3/Condicionais - Questão 1.py
@@ -0,0 +1,6 @@
+idade_juliana = int(input("Digite a idade da Juliana: "))
+idade_cris = int(input("Digite a idade da Cris: "))
+
+pode_entrar = idade_juliana > 17 and idade_cris > 17
+
+print(pode_entrar)
